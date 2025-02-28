9f3c0ad2e91619459b62978f30d10a3130cdeab8
davideblaffard <infodavide98@gmail.com>
2025-02-28 17:00:59 +0100

    init: created repo

---
+++ src/main/java/org/lessons/java/spring/pizzeria/spring_la_mia_pizzeria_crud_application/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/spring/pizzeria/spring_la_mia_pizzeria_crud_application/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
