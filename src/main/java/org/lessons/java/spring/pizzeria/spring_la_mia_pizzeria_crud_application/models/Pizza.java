27643cf4bbcb93fc42f2e18be8e9420d22cb1f42
davideblaffard <infodavide98@gmail.com>
2025-03-01 17:31:22 +0100

    add: added Pizza entity

---
+++ src/main/java/org/lessons/java/spring/pizzeria/spring_la_mia_pizzeria_crud_application/models/Pizza.java
@@ -0,0 +1,67 @@
+package org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String descrizione;
+    private String fotoUrl;
+    private double prezzo;
+
+    public Pizza(String nome, String descrizione, String fotoUrl, double prezzo){
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.fotoUrl = fotoUrl;
+        this.prezzo = prezzo;
+    }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getFotoUrl() {
+        return this.fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public double getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+
+}
