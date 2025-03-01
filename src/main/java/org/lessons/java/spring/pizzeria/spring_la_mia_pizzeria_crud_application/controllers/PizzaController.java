2b79f0d5d2181cad0ff5f34f7176bca321bcd29c
davideblaffard <infodavide98@gmail.com>
2025-03-01 17:57:24 +0100

    add: added PizzaController

---
+++ src/main/java/org/lessons/java/spring/pizzeria/spring_la_mia_pizzeria_crud_application/controllers/PizzaController.java
@@ -0,0 +1,27 @@
+package org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application.controllers;
+
+import org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application.models.Pizza;
+import org.lessons.java.spring.pizzeria.spring_la_mia_pizzeria_crud_application.repository.PizzaRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+
+@Controller
+public class PizzaController {
+    private final PizzaRepository pizzaRepository;
+
+    public PizzaController(PizzaRepository pizzaRepository){
+        this.pizzaRepository = pizzaRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<Pizza> pizzas = pizzaRepository.findAll();
+        model.addAttribute("pizzas", pizzas);
+        return "index";
+    }
+}
